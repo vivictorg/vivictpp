aff314c377064191e81885542ff3829049d201c0
Gustav Grusell <gustav.grusell@gmail.com>
2023-05-14 20:31:03 +0200

    ci: update get-version.sh
    Signed-off-by: Gustav Grusell <gustav.grusell@gmail.com>

--- get-version.sh
+++ get-version.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 description=$(git describe)
-if [[ $description =~ ^v[0-9]+[.][0-9]+[.][0-9]+$ ]]
+if [[ $description =~ ^v[0-9]+[.][0-9]+[.][0-9]+(-.*)?$ ]]
 then
-    echo "$description" | sed -e 's/v//g'
-elif [[ $description =~ ^v[0-9]+[.][0-9]+[.][0-9]+-.*$ ]]
-then
-     echo "$description" | sed -re 's/v([0-9]+[.][0-9]+[.][0-9]+)(-[a-z0-9]+)?(-([0-9]+)-[0-9a-z]+)/\1.\4/g'
+    echo "${description%%-*}" | sed -e 's/v//g'
 else
     exit 1
 fi
